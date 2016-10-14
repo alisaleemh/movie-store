8c9c39187e6d0f1bfb94676fbbc7afb14b347a16
alisaleemh <ali.s1995@gmail.com>
2016-10-13 22:29:08 -0400

    updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # udacity-movieTrailerp1
+
+Instructions
+-----------------
+
+# Make sure Python 2.7 is installed
+- https://www.python.org/downloads/
+
+# Clone Git Repositor
+- `$ git clone https://github.com/p00gz/udacity-fullstack-movie-trailer-website`
+
+# Run media.py
+- `python media.py`
--- movies.py
+++ movies.py
@@ -2,12 +2,18 @@ import media
 import webbrowser
 import fresh_tomatoes
 
-dch = media.Movie("Dil Chahta Hay","A funny story","https://en.wikipedia.org/wiki/Dil_Chahta_Hai","https://www.youtube.com/watch?v=0pzF-MbiiZ0")
+#declaration of movie objects
+ltr1 = media.Movie("The Lord of the Rings: Fellowship of the Rings","Quest to destroy the one ring","https://upload.wikimedia.org/wikipedia/en/9/9d/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_%282001%29_theatrical_poster.jpg","https://www.youtube.com/watch?v=V75dMMIW2B4")
+ltr2 = media.Movie("The Lord of The Rings: The Twin Towers","Battle of Helms Deep","https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg","https://www.youtube.com/watch?v=cvCktPUwkW0")
+ltr3 = media.Movie("The Lord of The Rings: Fellowship of the Ring","Quest to destroy the one ring","https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg","https://www.youtube.com/watch?v=r5X-hFf6Bwo")
+hb1 = media.Movie("The Lord of The Rings: Fellowship of the Ring","Quest to destroy the one ring","","")
+hb2 = media.Movie("The Lord of The Rings: Fellowship of the Ring","Quest to destroy the one ring","","")
+hb3 = media.Movie("The Lord of The Rings: Fellowship of the Ring","Quest to destroy the one ring","","")
 
-pch = media.Movie("The Lord of The Rings: Fellowship of the Ring","Quest to destroy the one ring","https://en.wikipedia.org/wiki/Dil_Chahta_Hai","https://www.youtube.com/watch?v=0pzF-MbiiZ0")
+
 
 movieList=[];
 
-movieList=[dch,pch]
+movieList=[ltr1,ltr2,ltr3]
 
 fresh_tomatoes.open_movies_page(movieList)
